fc4d005f905a3d335057d8e67670a6b4d3215e7a
DewP <parinsura@gmail.com>
2021-11-23 21:24:21 +0700

    Add undo button

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uuid/uuid.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -11,29 +12,38 @@ class MapPage extends StatefulWidget {
 }
 
 class _State extends State<MapPage> {
+  final uuid = const Uuid();
   final LatLng _center = const LatLng(13.731283160553428, 100.5416772151501);
 
   final List<LatLng> _currentPoints = [];
 
   Set<Marker> get _markers => HashSet.from(_currentPoints
       .map<Marker>((point) => Marker(
-            markerId: MarkerId(point.hashCode.toString()),
+            markerId: MarkerId(uuid.v1()),
             position: point,
           ))
       .toList());
 
   Set<Polygon> get _polygons => HashSet.from([
-    Polygon(
-      polygonId: const PolygonId('myPolygon'),
-      points: _currentPoints,
-      strokeWidth: 2,
-      fillColor: Colors.yellow.withOpacity(0.4),
-    )
-  ]);
+        Polygon(
+          polygonId: PolygonId(uuid.v1()),
+          points: _currentPoints,
+          strokeWidth: 2,
+          fillColor: Colors.yellow.withOpacity(0.4),
+        )
+      ]);
+
+  bool get _hasPointOnScreen => _currentPoints.isNotEmpty;
 
   void _onTapMapPoint(LatLng location) {
     setState(() {
       _currentPoints.add(location);
+    });
+  }
+
+  void _onTapUndo() {
+    setState(() {
+      _currentPoints.removeLast();
     });
   }
 
@@ -52,6 +62,12 @@ class _State extends State<MapPage> {
         onTap: _onTapMapPoint,
         markers: _markers,
         polygons: _polygons,
+        myLocationButtonEnabled: false,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _hasPointOnScreen ? _onTapUndo : null,
+        child: const Icon(Icons.undo),
+        backgroundColor: Colors.black,
       ),
     );
   }
